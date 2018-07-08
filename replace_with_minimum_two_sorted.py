2aa4607bd70dd0ff5aa2d61842ed73bb0c4ab576
pranavdave893 <pranavdave893@gmail.com>
2018-07-07 21:44:50 -0700

    Add files via upload

---
+++ replace_with_minimum_two_sorted.py
@@ -0,0 +1,37 @@
+x = [3,-1,5,-1,8,-1,12]
+b = [2,7,10]
+
+j = len(x)-1
+k = j
+
+while(j>=0):
+    if x[j]<0:
+        j = j-1
+    if j < 0 :
+        break
+    if (x[j] > 0 and x[k]<0):
+        x[j],x[k] = x[k],x[j]
+        k = k-1
+    else:
+        j = j-1
+        if not x[k]<0:
+            k = k-1
+
+for i in range(len(x)-1):
+    if x[i]>0:
+        j = i
+        break
+i=0
+k=0
+while i<=len(b)-1:
+    if b[i] < x[j]:
+        x[k] = b[i]
+        k += 1
+        i += 1
+    if i >= len(b):
+        break
+    if x[j] < b[i]:
+        x[k] = x[j]
+        j += 1
+        k += 1
+print x

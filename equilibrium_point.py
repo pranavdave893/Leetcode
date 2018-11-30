4796cbbb3c32071d797c1bb24312871f5a590a72
Pranav <pranavdave893@gmail.com>
2018-11-29 18:47:04 -0800

    Equilibrium point

--- equilibrium_point.py
+++ equilibrium_point.py
@@ -1,34 +1,37 @@
 #code
-t = int(input())
-while (t !=0):
-    _ = int(input())
-    x = list(map(int,input().strip(' ').split(' ')))
+def getSolution(x,n):
     i = 0
-    j = len(x) - 1
+    j = n - 1
     
-    if j == 1:
-        print (-1)
-        break
+    if j == 0:
+        return x[j]
     l_sum = x[i]
     r_sum = x[j]
     
-    while(i != j or j<i or i>j):
+    while(i != j and i <j):
         if(l_sum > r_sum):
             j -= 1
             r_sum += x[j]
-        
         if(l_sum < r_sum):
             i += 1
             l_sum += x[i]
-        
-        if(l_sum == r_sum and i!=j):
-            i += 1
-            j -= 1
-            if (i==j):
-                print (i)
-                break
-            else:
-                l_sum += x[i]
-                r_sum += x[j]
-    print (-1)
+        try:
+            if(l_sum == r_sum and i!=j):
+                i += 1
+                j -= 1
+                if (i==j):
+                    return i+1
+                else:
+                    l_sum += x[i]
+                    r_sum += x[j]
+        except:
+            import pdb; pdb.set_trace()
+    return -1
+
+t = int(input())
+while(t !=0):
+    n = int(input())
+    x = list(map(int,raw_input().strip(' ').split(' ')))
+    solution = getSolution(x,n)
+    print (solution)
     t = t-1

360f7d491e06d27d28e74ad525af52f0d35e75c8
Pranav Dave <pranavdave893@gmail.com>
2019-11-03 09:58:37 -0800

    combination sum

--- combination_sum_4.py
+++ combination_sum_4.py
@@ -23,12 +23,28 @@ class Solution(object):
             
                 dp[target] = count
             return count
-                    
+    
         count = dp_recur(nums, target, dp)
-        print (dp)
+        # print (dp)
         
         return count
+    
+    def combinationSum4_bottomup(self, nums, target):
+        dp = [0] * (target+1)
+        dp[0] = 1
+
+        # This is the same thing we do in for loop and recursion.
+        for i in range(1, target+1):
+            for num in nums:
+                if i>=num:
+                    dp[i] += dp[i-num]
+        
+        return dp[target]
+                    
+        
 
 abc = Solution()
 print (abc.combinationSum4([1,2,3,4], 4))
 print (abc.combinationSum4([3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25], 10))
+print (abc.combinationSum4_bottomup([1,2,3,4], 4))
+print (abc.combinationSum4_bottomup([3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25], 10))

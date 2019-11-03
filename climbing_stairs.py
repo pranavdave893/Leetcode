fc7a0b771694261313983294ea24626c1845cd80
Pranav Dave <pranavdave893@gmail.com>
2019-11-03 09:57:58 -0800

    climb stairs dp

--- climbing_stairs.py
+++ climbing_stairs.py
@@ -13,10 +13,14 @@ class Solution:
 
         if n < 0 : return 0
 
-        base = [0,1,2]
-
+        base = {1:1, 2:2}
         if base == 1: return 1
         if base == 2: return 2
+
+        for i in range(3, n+1):
+            base[i] = base[i-1] + base[i-2]
+        
+        return base[n]
         
 abc = Solution()
 print abc.climbStairs(15)
